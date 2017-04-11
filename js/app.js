57a1e7c0f5caeac9083ee471ec45cdd6bde4c592
Daniel Siwczak <dsiwczak@o2.pl>
2017-04-11 10:47:04 +0200

    add request to github

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-  var app = angular.module( "app" , [] );
+$(document).ready(function() {
+  $('#searchUser').on('keyup' , function(e) {
+    let username = e.target.value;
 
-  app.controller
+    // Reguest to github
+    $.ajax({
+      url: 'https://api.github.com/users/'+username,
+      data: {
+        client_id: '0aa2918f3d449c0fc38d',
+        client_secret: 'c67ac3cb2dda14b256b59b0b45c7929b191ac251',
+      }
+    }).done(function(user) {
+      $('#profile').html(`
+        ${user.name}
+      `);
+    });
+  });
+});
