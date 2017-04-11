5c04deb62670f3c1ad44c542c0880784dcaf56fb
Daniel Siwczak <dsiwczak@o2.pl>
2017-04-11 19:30:08 +0200

    add profile button

--- js/app.js
+++ js/app.js
@@ -18,17 +18,17 @@ $(document).ready(function() {
             <tr class="item">
               <td><img src="${user.avatar_url}" /></td>
               <td>${index+1}</td>
-              <td id="userName">${user.login}</td>
+              <td class="userName">${user.login}</td>
               <td><span class="label label-default">${user.contributions}</span></td>
               <td><span id="userRepos" class="label label-primary"></span></td>
               <td><span id="userGists" class="label label-primary"></span></td>
               <td><span id="userFollowers" class="label label-info"></span></td>
               <td><span id="userFollowing" class="label label-info"></span></td>
+              <td><a href="${user.html_url}" class="btn btn-warning" target="_blank">View Profile</a></td>
             </tr>             
       `);      
     });
-      
-      let username = document.getElementById("userName").innerHTML;
+      var username = document.getElementsByClassName("userName")[1].innerHTML;
       console.log(username);
       $.ajax({
         url: 'https://api.github.com/users/' + username,
@@ -37,10 +37,10 @@ $(document).ready(function() {
         client_secret: 'c67ac3cb2dda14b256b59b0b45c7929b191ac251',
       }
     }).done(function(info) {
-        $('#userRepos').append(`<span>${info.public_repos}</span>`)
-        $('#userGists').append(`<span>${info.public_gists}</span>`)
-        $('#userFollowers').append(`<span>${info.followers}</span>`)
-        $('#userFollowing').append(`<span>${info.following}</span>`)
+        $('#info #userRepos').append(`<span>${info.public_repos}</span>`)
+        $('#info #userGists').append(`<span>${info.public_gists}</span>`)
+        $('#info #userFollowers').append(`<span>${info.followers}</span>`)
+        $('#info #userFollowing').append(`<span>${info.following}</span>`)
       })
   });
 });
